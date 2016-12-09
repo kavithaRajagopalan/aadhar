dca6a92e8d0c605c67eb5f7fa615302184bd5f78
KavithaR <kavithar@thoughtworks.com>
2016-12-09 19:37:56 +0530

    Generates aadhar number on person creation

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,8 @@
 class Person < ActiveRecord::Base
+  before_save :generate_aadhar_number
+
+  private
+  def generate_aadhar_number
+    self.aadharNum = (rand(10 ** 11) + Time.now.hash.abs.to_s[0..8].to_i).to_s
+  end
 end
