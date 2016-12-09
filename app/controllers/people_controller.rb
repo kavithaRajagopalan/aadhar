7f1b0fee2bf87994b5ceae04fd3cb4dad8a94fe6
KavithaR <kavithar@thoughtworks.com>
2016-12-09 19:00:42 +0530

    Redirects to person's show page after being successfully added; can also list all registered people

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,15 +1,21 @@
 class PeopleController < ApplicationController
 
-  def new
-
-  end
-  
   def create
     @person = Person.new(sanitise_params)
 
     @person.save
 
+    redirect_to @person
   end
+
+  def show
+    @person = Person.find(params[:id])
+  end
+
+  def index
+    @people = Person.all
+  end
+  
 
   private
   def sanitise_params
